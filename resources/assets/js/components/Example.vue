f5455b9c0a2c6c0bf40cc7fbbcbf7b2324e61158
wangqi <331210877@qq.com>
2017-10-12 21:04:05 +0800

    base

---
+++ resources/assets/js/components/Example.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Example Component</div>
+                    <el-radio class="radio" v-model="radio" label="1">备选项</el-radio>
+                    <div class="panel-body">
+                        I'm an example component!666
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data () {
+          return {
+            radio: '1'
+          }
+        },
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
